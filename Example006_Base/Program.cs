995aaec488eb91d4aafaaddbec4e40d4cc29d207
Suraikin Vladislav <Ghost845JB@gmail.com>
2022-09-15 22:29:47 +0300

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 35;
+int b = 23;
+int c = 234;
+int d = 213;
+int e = 7;
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
